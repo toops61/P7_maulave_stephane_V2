00aa4c308d254f11c5bd70aaf74e4a3dda8fe205
Toops61 <toops61@gmail.com>
2023-08-23 11:42:48 +0200

    fix name

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/style.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
